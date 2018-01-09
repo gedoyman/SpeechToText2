0280eb42258f983c97befcc276eecc585a75e79c
gedoyman <hendra.yani28@gmail.com>
2018-01-09 12:10:07 +0700

    Initial commit

---
+++ app/src/main/java/com/littleaozora/hendra/speechtotext2/MainActivity.java
@@ -0,0 +1,217 @@
+package com.littleaozora.hendra.speechtotext2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    Button btnStart, btnShare, btnClear;
+    TextView txtResult;
+    ProgressBar prgBar;
+    private boolean btnActive=false;
+    SpeechRecognizer mSpeechRecognizer;
+    Intent mSpeechRecognizerIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnShare = (Button) findViewById(R.id.btnShare);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtResult.setMovementMethod(new ScrollingMovementMethod());
+        prgBar = (ProgressBar) findViewById(R.id.prgBar);
+
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "id");
+        //mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 60000); // value to wait
+        //mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 60000);
+        //mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 60000);
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+                //continueListening();
+                Log.d(TAG, "onError : "+error);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        continueListening();
+                    }
+                },100);
+
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+
+                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if(matches != null){
+                    String before = txtResult.getText().toString();
+                    String output = matches.get(0).substring(0, 1).toUpperCase() + matches.get(0).substring(1);
+                    txtResult.setText(before+" "+output+". ");
+                    Log.d(TAG, "onResult = "+matches.get(0));
+                }else{
+                    Log.d(TAG, "onResult Null = "+matches.get(0));
+                }
+                continueListening();
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnActive == false){
+                    btnActive = true;
+                    btnStart.setText("Stop");
+                    mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                    prgBar.setVisibility(View.VISIBLE);
+                }else{
+                    mSpeechRecognizer.stopListening();
+                    btnActive = false;
+                    btnStart.setText("Start");
+                    prgBar.setVisibility(View.GONE);
+                }
+                Log.d(TAG, "OnClick: "+btnActive);
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Confirmation");
+                alertDialog.setIcon(R.drawable.ic_dialog_alert);
+                alertDialog.setMessage("Do you realy want to clear text ?");
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                mSpeechRecognizer.stopListening();
+                                btnActive = false;
+                                prgBar.setVisibility(View.GONE);
+                                btnStart.setText("Start");
+
+                                dialog.dismiss();
+                                txtResult.setText("");
+                            }
+                        });
+
+                alertDialog.show();
+            }
+        });
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSpeechRecognizer.stopListening();
+                btnActive = false;
+                prgBar.setVisibility(View.GONE);
+                btnStart.setText("Start");
+
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = txtResult.getText().toString();
+                String shareSubject = "Speech to Text2";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent, "Share using"));
+            }
+        });
+    }
+
+
+
+    private void checkPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)){
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:"+getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
+    private void continueListening(){
+        if(btnActive == true){
+            Log.d(TAG, "continueListening");
+            //mSpeechRecognizer.stopListening();
+            mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/littleaozora/hendra/speechtotext2/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.littleaozora.hendra.speechtotext2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logo;
+    Animation animFadeIn;
+    TextView textLogo;
+    private static int SPLASH_TIME_OUT = 6000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = (ImageView) findViewById(R.id.logo);
+        textLogo = (TextView) findViewById(R.id.textLogo);
+        animFadeIn = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        logo.startAnimation(animFadeIn);
+        textLogo.startAnimation(animFadeIn);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
